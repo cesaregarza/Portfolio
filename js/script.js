28f722cbaa26f2e6c9d9c0e8672b0f509872da7e
cesaregarza <cesar.garza.826@gmail.com>
2018-06-29 17:35:43 -0500

    Updated animations, abandoned slides

--- js/script.js
+++ js/script.js
@@ -1,13 +1,28 @@
 var controller = new ScrollMagic.Controller();
 
+//Pin Navbar to sticky
 $(function () {
   var scene = new ScrollMagic.Scene({
-      triggerElement: `#navtrigger`,
+      triggerElement: `#navstart`,
       reverse: true,
       triggerHook: 0
     }).setPin(`#navstart`)
+    .addIndicators()
     .addTo(controller);
+  
+    // Move navbar from its initial position to the correct one
+    var tween0 = new TimelineMax()
+      .add(TweenMax.fromTo("#navstart", 1, {y:"-100%"}, {y: "0%", ease: Linear.easeNone}));
 
+    var scene1 = new ScrollMagic.Scene({
+      triggerElement: "#parallax1",
+      duration: $(window).height(),
+      triggerHook: 0
+    }).setTween(tween0)
+      .addIndicators()
+      .addTo(controller);
+
+// Jumbotron Parallax Effect
   var scene2 = new ScrollMagic.Scene({
       triggerElement: "#parallax1",
       duration: "200%",
@@ -19,12 +34,38 @@ $(function () {
     })
     .addTo(controller);
 
+// Navbar triggers
   var scene3 = new ScrollMagic.Scene({
-      triggerElement: "#navtrigger",
-      triggerHook: 0.25
+      triggerElement: "#aboutme",
+      triggerHook: 0.25,
+      duration: "100%"
     }).setClassToggle("#aboutmenav", "active")
+    .addIndicators()
     .addTo(controller);
 
+  var scene4 = new ScrollMagic.Scene({
+    triggerElement: "#portfolio",
+    triggerHook: 0.25,
+  }).setClassToggle("#portfolionav", "active")
+    .addIndicators()
+    .addTo(controller);
+
+// // Slide animations
+//   var wipeAnimation = new TimelineMax()
+//     .fromTo("section.panel.panelone", 1, {y: "100%"}, {y: "0%", ease: Linear.easeNone})
+//     .fromTo("section.panel.paneltwo", 1, {y: "100%"}, {y: "0%", ease: Linear.easeNone});
+
+//   var scene5 = new ScrollMagic.Scene({
+//     triggerElement: "#pinContainer",
+//     triggerHook: 0,
+//     duration: "300%"
+//   })
+//     .setPin("#pinContainer")
+//     .setTween(wipeAnimation)
+//     .addIndicators()
+//     .addTo(controller);
+
+    //Smooth Scroll
   controller.scrollTo(function (newpos) {
     TweenMax.to(window, 0.3, {
       scrollTo: {
