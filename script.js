faa5fa249fcc54e4ab3b55bc20f460f2e7f3b180
cesaregarza <cesar.garza.826@gmail.com>
2018-06-28 20:03:52 -0500

    Parallax Jumbotron and Sticky Nav

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
 var controller = new ScrollMagic.Controller();
 
-var scene = new ScrollMagic.Scene({
-  offset: 100,
-  duration: 400
+$(function () {
+  var scene = new ScrollMagic.Scene({
+      triggerElement: `#navtrigger`,
+      reverse: true,
+      triggerHook: 0
+    }).setPin(`#navstart`)
+    .addTo(controller);
+
+  var scene2 = new ScrollMagic.Scene({
+      triggerElement: "#parallax1",
+      duration: "200%",
+      triggerHook: "onEnter",
+    })
+    .setTween("#parallax1 > div", {
+      y: "80%",
+      ease: Linear.easeNone
+    })
+    .addTo(controller);
+
+
 });
